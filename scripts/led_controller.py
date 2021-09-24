9ad11a37e64b700dc7a1fd8d40f1033f0e0dcd39
Floris <floris@ryberg.com>
2021-09-24 10:38:50 +0200

    fix error in led controller, works on experimental setu

--- scripts/led_controller.py
+++ scripts/led_controller.py
@@ -2,7 +2,7 @@
 
 import enum
 import rospy
-from std_msgs.msg import String, Int16
+from std_msgs.msg import String, Int8
 from std_srvs.srv import Empty
 from diagnostics_srvs.srv import TurnLedOn, TurnLedOnRequest, TurnLedRed
 
@@ -24,46 +24,46 @@ class LedController():
         self.turn_led_red(req.led_name)
         
     def turn_led_red(self, led_name):
-        self.turn_led_on_request.led_name = led_name
-        self.turn_led_on_request.r = 255
-        self.turn_led_on_request.g = 0
+        self.turn_led_on_request.led_name = String(led_name)
+        self.turn_led_on_request.r = Int8(255)
+        self.turn_led_on_request.g = Int8(0)
 
         response = self.turn_led_on(self.turn_led_on_request)
 
-    def turn_led_green(self, led_name):
-        self.turn_led_on_request.led_name = led_name
-        self.turn_led_on_request.r = 0
-        self.turn_led_on_request.g = 255
+    def turn_led_green(self, led_name):        
+        self.turn_led_on_request.led_name = String(led_name)
+        self.turn_led_on_request.r = Int8(0)
+        self.turn_led_on_request.g = Int8(255)
 
         response = self.turn_led_on(self.turn_led_on_request)
     
     def turn_led_orange(self, led_name):
-        self.turn_led_on_request.led_name = led_name
-        self.turn_led_on_request.r = 255
-        self.turn_led_on_request.g = 255
+        self.turn_led_on_request.led_name = String(led_name)
+        self.turn_led_on_request.r = Int8(255)
+        self.turn_led_on_request.g = Int8(255)
 
         response = self.turn_led_on(self.turn_led_on_request)
     
     def turn_led_off(self, led_name):
-        self.turn_led_on_request.led_name = led_name
-        self.turn_led_on_request.r = 0
-        self.turn_led_on_request.g = 0
+        self.turn_led_on_request.led_name = String(led_name)
+        self.turn_led_on_request.r = Int8(0)
+        self.turn_led_on_request.g = Int8(0)
 
         response = self.turn_led_on(self.turn_led_on_request)
     
-    def turn_all_leds_off(self):
+    def turn_all_leds_off(self, req):
         for led_name in self.leds:
             self.turn_led_off(led_name)
 
-    def turn_all_leds_orange_handler(self):
+    def turn_all_leds_orange_handler(self, req):
         for led_name in self.leds:
             self.turn_led_orange(led_name)
 
-    def turn_all_leds_green_handler(self):
+    def turn_all_leds_green_handler(self, req):
         for led_name in self.leds:
             self.turn_led_green(led_name)
     
-    def turn_front_leds_red_handler(self):
+    def turn_front_leds_red_handler(self, req):
         self.turn_led_red("A1")
         self.turn_led_red("A5")
         self.turn_led_red("B5")
